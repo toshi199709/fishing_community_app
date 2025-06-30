dab018d35ef9480785bfe12f4f6e39ff16f9f610
toshi199709 <ayatoshi077@gmail.com>
2025-06-30 15:57:16 +0900

    メール削除・ニックネーム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,3 +13,11 @@ def email_changed?
   false
 end
 
+def self.find_for_database_authentication(warden_conditions)
+  conditions = warden_conditions.dup
+  if nickname = conditions.delete(:nickname)
+    where(conditions.to_h).where(["lower(nickname) = :value", { value: nickname.downcase }]).first
+  else
+    where(conditions.to_h).first
+  end
+end
