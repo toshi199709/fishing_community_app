7f881196081eec154aef9c7c7d0dad043a442f30
toshi199709 <ayatoshi077@gmail.com>
2025-06-30 17:27:05 +0900

    投稿フォームの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,26 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "投稿が完了しました！"
+    else
+      render :new
+    end
   end
 
-  def index
+  private
+
+  def post_params
+    params.require(:post).permit(:text, :image)
   end
 end
