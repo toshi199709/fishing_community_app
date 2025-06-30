55e05fbe31f3be8a4c11f9dfc53fc9e98d19caa9
toshi199709 <ayatoshi077@gmail.com>
2025-06-30 17:12:33 +0900

    投稿画面の準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,25 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Devise モジュール
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
 
-def email_required?
-  false
-end
+  # 投稿とのアソシエーション
+  has_many :posts, dependent: :destroy
 
-def email_changed?
-  false
-end
+  def email_required?
+    false
+  end
 
-def self.find_for_database_authentication(warden_conditions)
-  conditions = warden_conditions.dup
-  if nickname = conditions.delete(:nickname)
-    where(conditions.to_h).where(["lower(nickname) = :value", { value: nickname.downcase }]).first
-  else
-    where(conditions.to_h).first
+  def email_changed?
+    false
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if nickname = conditions.delete(:nickname)
+      where(conditions.to_h).where(["lower(nickname) = :value", { value: nickname.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
   end
 end
